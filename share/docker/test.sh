86fa8bd12cbc2ba78ca5141fcc70dfd7f2b5d8d8
Hugo Freire <hfreire@exec.sh>
2019-02-03 15:27:39 +0100

    chore(docker): add docker support

---
+++ share/docker/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./start.sh &
+
+sleep 10
+
+curl \
+--silent \
+--fail \
+--connect-timeout 5 \
+--max-time 10 \
+--retry 5 \
+--retry-delay 5 \
+--retry-max-time 60 \
+http://localhost:3000 >/dev/null 2>&1 || exit 1
